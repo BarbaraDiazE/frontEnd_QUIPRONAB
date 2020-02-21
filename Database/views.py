f17d50ee09fc9c6734f33b32527cd9047603eced
BarbaraDiazE <debi_1223@hotmail.com>
2020-02-21 17:44:46 -0600

    gitignore

---
+++ Database/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+
+from rest_framework.views import APIView
+
+import os, glob, csv
+
+# Create your views here.
+
+
+class ServerViews(APIView):
+    def get(self, request):
+        return render(request, "home.html")
+
