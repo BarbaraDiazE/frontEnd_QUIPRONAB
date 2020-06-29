328aaf2f23432d1906dc39cb7901d69f4d4ad474
Barbara <debi_1223@hotmail.com>
2020-06-29 10:14:35 -0500

    some kekule.js implementations

--- Database/views.py
+++ Database/views.py
@@ -12,19 +12,31 @@ class ServerViews(APIView):
     def get(self, request):
         return render(request, "home.html")
 
+
 class A(APIView):
     def get(self, request):
         return render(request, "chemViewer2D.html")
 
+
 class B(APIView):
     def get(self, request):
-        return render(request, "renderStatic.html")
+        mol_path = "chemFiles/pyrrole.mol"
+        ctx = {
+            "mol_location": f"url(../static/{mol_path})",
+        }
+        return render(request, "renderStatic.html", context=ctx)
+
 
 class D(APIView):
     def get(self, request):
         return render(request, "render3D.html")
 
+
+class InputChemStruc(APIView):
+    def get(self, request):
+        return render(request, "compose.html")
+
+
 class About(APIView):
     def get(self, request):
         return render(request, "about.html")
-
